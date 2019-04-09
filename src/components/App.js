a7ffb0d2669c94c67f6a24844db422facbe11292
Diego Lottermann <diegolottermann@Diegos-MacBook-Air.local>
2019-04-08 22:27:57 -0300

    Begin Project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+         <h3>My project</h3> 
+      </div>
+    );
+  }
+}
+
+export default App;
